00a947baae04eb183c22c788a94d326533903319
김성은 <kse012050@naver.com>
2025-04-14 04:44:26 +0900

    마우스 effect, resize

--- CSSeffect/Franks Iaboratory/파티클 텍스트/script.js
+++ CSSeffect/Franks Iaboratory/파티클 텍스트/script.js
@@ -1,7 +1,9 @@
 window.addEventListener('load', function(){
     // const textInput = document.getElementById('textInput');
     const canvas = document.getElementById('canvas1');
-    const ctx = canvas.getContext('2d');
+    const ctx = canvas.getContext('2d', {
+        willReadFrequently: true,
+    });
 
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
@@ -18,7 +20,7 @@ window.addEventListener('load', function(){
             this.dx = 0;
             this.dy = 0;
             this.vx = 0;
-            this.dy = 0;
+            this.vy = 0;
             this.force = 0;
             this.angle = 0;
             this.distance = 0;
@@ -26,14 +28,24 @@ window.addEventListener('load', function(){
             this.ease = Math.random() * 0.1 + 0.0005;
         }
         draw(){
-            // console.log(this.y);
-            
             this.effect.context.fillStyle = this.color;
             this.effect.context.fillRect(this.x, this.y, this.size, this.size);
         }
         update(){
-            this.x += (this.originX - this.x) * this.ease;
-            this.y += (this.originY - this.y) * this.ease;
+            this.dx = this.effect.mouse.x - this.x;
+            this.dy = this.effect.mouse.y - this.y;
+            // this.distance = Math.sqrt(this.dx * this.dx + this.dy * this.dy);
+            this.distance = this.dx * this.dx + this.dy * this.dy;
+            this.force = -this.effect.mouse.radius / this.distance;
+
+            if(this.distance < this.effect.mouse.radius){
+                this.angle = Math.atan2(this.dy, this.dx);
+                this.vx += this.force * Math.cos(this.angle);
+                this.vy += this.force * Math.sin(this.angle);
+            }
+
+            this.x += (this.vx *= this.friction) + (this.originX - this.x) * this.ease;
+            this.y += (this.vy *= this.friction) + (this.originY - this.y) * this.ease;
         }
     }
 
@@ -44,10 +56,11 @@ window.addEventListener('load', function(){
             this.canvasHeight = canvasHeight;
             this.textX = this.canvasWidth / 2;
             this.textY = this.canvasHeight / 2;
-            this.fontSize = 80;
-            this.lineHeight = this.fontSize * 0.8;
+            this.fontSize = 200;
+            this.lineHeight = this.fontSize * 1.1;
             this.maxTextWidth = this.canvasWidth * 0.8;
             this.textInput = document.getElementById('textInput');
+            this.verticalOffset = 0; 
             this.textInput.addEventListener('keyup', (e) => {
                 if(e.key !== ' '){
                     this.context.clearRect(0, 0, this.canvasWidth, this.canvasHeight);
@@ -71,14 +84,16 @@ window.addEventListener('load', function(){
         wrapText(text){
             const gradient = this.context.createLinearGradient(0, 0, this.canvasWidth, this.canvasHeight);
             gradient.addColorStop(0.3, 'red')
-            gradient.addColorStop(0.5, 'fuchsia')
-            gradient.addColorStop(0.7, 'purple')
+            gradient.addColorStop(0.5, 'orange')
+            gradient.addColorStop(0.7, 'yellow')
             this.context.fillStyle = gradient;
             this.context.textAlign = 'center';
             this.context.textBaseline = 'middle';
             this.context.lineWidth = 3;
             this.context.strokeStyle = 'white';
-            this.context.font = this.fontSize + 'px helvetica';
+            this.context.letterSpacing = '5px';
+            // this.context.font = this.fontSize + 'px Bangers';
+            this.context.font = this.fontSize + 'px Impact';
             // this.context.fillText(text, this.textX, this.textY)
             // this.context.strokeText(text, this.textX, this.textY)
             let lineArray = [];
@@ -96,7 +111,7 @@ window.addEventListener('load', function(){
                 lineArray[lineCounter] = line;
             }
             let textHeight = this.lineHeight * lineCounter;
-            this.textY = this.canvasHeight / 2 - textHeight / 2;
+            this.textY = this.canvasHeight / 2 - textHeight / 2 + this.verticalOffset;
             
             lineArray.forEach((line, index) => {
                 this.context.fillText(line, this.textX, this.textY + (index * this.lineHeight));
@@ -129,10 +144,18 @@ window.addEventListener('load', function(){
                 particle.draw();
             })
         }
+        resize(width, height){
+            this.canvasWidth = width;
+            this.canvasHeight = height;
+            this.textX = this.canvasWidth / 2;
+            this.textY = this.canvasHeight / 2;
+            this.maxTextWidth = this.canvasWidth * 0.8;
+        }
     }
 
     const effect = new Effect(ctx, canvas.width, canvas.height);
     // effect.wrapText('Hello how are you')
+    effect.wrapText(effect.textInput.value);
     effect.render()
     
     function animate(){
@@ -141,6 +164,13 @@ window.addEventListener('load', function(){
         requestAnimationFrame(animate);
     }
     animate();
+
+    window.addEventListener('resize', function(){
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerHeight;
+        effect.resize(canvas.width, canvas.height);
+        effect.wrapText(effect.textInput.value);
+    })
 
     // ctx.lineWidth = 3;
     // ctx.strokeStyle = 'red';
